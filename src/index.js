e0503aa8334155ccc4eadc0565fa0b974bfa43e6
Liz3 <accs@21xayah.com>
2020-02-02 01:05:44 +0100

    fix copy bug. add timeout option. disable download

--- src/index.js
+++ src/index.js
@@ -6,45 +6,54 @@ console.log(`Starting MC_Bootstrap ${config.version}, ${systemUtils.getWd()}`);
 const args = process.argv.slice(2);
 const help = () => {
   console.log("MC Bootstrap help");
-  console.log("Usage: mc-bootstrap build/run/help [...args]");
-  console.log(`\tbuild`);
-  console.log(
-    `\t\t--out,-o PATH : Define the directoy where the server will be created`
-  );
-  console.log(
-    `\t\t--server,-s VERSION : Define the server version, can be one of ${config.server_versions.join(
-      ","
-    )}`
-  );
-  console.log(
-    `\t\t--bin,-b PATH(default to "java") : Define the java start executable (only for running)`
-  );
-  console.log(
-    `\t\t--force,-f true/false : Force create, otherwise action will be aborted should the server directory exist`
-  );
-  console.log(
-    `\t\t--template,-t PATH : Copies all .jar files from this directory into the plugins directory of the target folder`
-  );
+  console.log("Usage: mc-bootstrap build/help [...args]");
+  // console.log(`\tbuild`);
+  // console.log(
+  //   `\t\t--out,-o PATH : Define the directoy where the server will be created`
+  // );
+  // console.log(
+  //   `\t\t--server,-s VERSION : Define the server version, can be one of ${config.server_versions.join(
+  //     ","
+  //   )}`
+  // );
+  // console.log(
+  //   `\t\t--bin,-b PATH(default to "java") : Define the java start executable (only for running)`
+  // );
+  // console.log(
+  //   `\t\t--force,-f true/false : Force create, otherwise action will be aborted should the server directory exist`
+  // );
+  // console.log(
+  //   `\t\t--template,-t PATH : Copies all .jar files from this directory into the plugins directory of the target folder`
+  // );
   console.log(`\rrun`);
-  console.log(`\t\t--dir,-d PATH : Define the directoy where the server root is`);
-  console.log(`\t\t--bin,-b PATH(default to "java") : Define the java start executable`);
+  console.log(
+    `\t\t--dir,-d PATH : Define the directoy where the server root is`
+  );
+  console.log(
+    `\t\t--bin,-b PATH(default to "java") : Define the java start executable`
+  );
   console.log(`\t\t--file,-f PATH : Define the server exeutable`);
   console.log(
     `\t\t--watch,-w : Define a file to be watched, this option is followed by 3 arguments, file(The file to be watch), dest(the destination file) and cmd()\n\t\t\tExample: -w myproject/build/plugin.jar myserver/plugins/plugin.jar restart\n\t\t\tActions: restart will perform a server restart, while anything else will only run the specified command`
   );
-  console.log(`\t\t--jvm,-j PARAM : Adds the following param to the JVM start args`);
-  console.log(`\t\t--dump,-o NAME : Rather then starting the configuration, it saves the configuration into a file`);
-  console.log(`\t\t--conf,-c NAME : Loads a configuration from the name and starts a server based on that config`);
+  console.log(
+    `\t\t--jvm,-j PARAM : Adds the following param to the JVM start args`
+  );
+  console.log(
+    `\t\t--dump,-o NAME : Rather then starting the configuration, it saves the configuration into a file`
+  );
+  console.log(
+    `\t\t--conf,-c NAME : Loads a configuration from the name and starts a server based on that config`
+  );
+  console.log(
+    `\t\t--timeout,-t NUMBER(default 3000) : Some build tools like maven or gradle write the target file more then once. to make sure only the newest version is copied. this defines in milliseconds how long to wait before actually copying the file, so only the final file gets copied to the server.`
+  );
 };
 if (args.length === 0) {
   console.log("No arguments provided!");
-  help()
+  help();
 } else {
   switch (args[0]) {
-    case "build": {
-      builder(args.slice(1), systemUtils.getWd());
-      break;
-    }
     case "run": {
       run(args.slice(1), systemUtils.getWd());
       break;
--- src/run.js
+++ src/run.js
@@ -20,6 +20,7 @@ module.exports = (args, wd) => {
     jvmArgs: [],
     watchers: [],
     fileName: null,
+    timeout: 3000
   };
   args.forEach((arg, index) => {
     if (arg === "--dir" || arg === "-d") {
@@ -33,15 +34,18 @@ module.exports = (args, wd) => {
     }
     if (arg === "--dump" || arg === "-o") {
       props.dump = true;
-      if (args[index + 1]) props.dumpPath =args[index + 1];
+      if (args[index + 1]) props.dumpPath = args[index + 1];
     }
     if (arg === "--conf" || arg === "-c") {
       if (args[index + 1]) props.confFile = args[index + 1];
     }
+    if (arg === "--timeout" || arg === "-t") {
+      if (args[index + 1]) props.timeout = Number.parseInt(props.timeout)
+    }
     if (arg === "--watch" || arg === "-w") {
       props.watchers.push({
         file: absoluteFolder(args[index + 1]),
-        out: absoluteFolder(args[index + 2]) ,
+        out: absoluteFolder(args[index + 2]),
         cmd: args[index + 3],
         md5Previous: null
       });
@@ -51,16 +55,27 @@ module.exports = (args, wd) => {
     }
   });
   if (propsValid(props) || props.confFile) {
-    if(props.dump) {
-      const dumpObject = {dir: props.dir, watchers: props.watchers, javaCmd: props.javaCmd, jvmArgs: props.jvmArgs, fileName: props.fileName}
+    if (props.dump) {
+      const dumpObject = {
+        dir: props.dir,
+        watchers: props.watchers,
+        javaCmd: props.javaCmd,
+        jvmArgs: props.jvmArgs,
+        fileName: props.fileName
+      };
       save(dumpObject, props.dumpPath);
       console.log("Saved config");
       return;
     }
-    if(props.confFile) {
-      console.log(`Loading config ${props.confFile}`)
-        const obj = load(props.confFile);
-        props = {...props, ...obj, watchers: [...props.watchers,...obj.watchers],jvmArgs: [...props.jvmArgs,...obj.jvmArgs]};
+    if (props.confFile) {
+      console.log(`Loading config ${props.confFile}`);
+      const obj = load(props.confFile);
+      props = {
+        ...props,
+        ...obj,
+        watchers: [...props.watchers, ...obj.watchers],
+        jvmArgs: [...props.jvmArgs, ...obj.jvmArgs]
+      };
     }
     console.log(`starting server ${props.fileName} in ${props.dir}`);
     let server = startServerWithPipes(
@@ -72,31 +87,38 @@ module.exports = (args, wd) => {
     );
     for (watch of props.watchers) {
       console.log(`Adding Watcher for ${watch.file}`);
+      if (!fs.existsSync(watch.out) && fs.existsSync(watch.file))
+        fs.copyFileSync(watch.file, watch.out);
       fs.watch(watch.file, (event, filename) => {
         if (filename && event === "change") {
           if (!fs.existsSync(watch.file)) return;
           const currentMd5 = md5(fs.readFileSync(watch.file));
           if (currentMd5 === watch.md5Previous) return;
-          watch.md5Previous = currentMd5;
-          restarting = true;
-          console.log(`${filename} file Changed`);
-          fs.copyFileSync(watch.file, watch.out);
-          if (watch.cmd === "restart") {
-            console.log("Restarting server...");
-            server.kill();
-            setTimeout(() => {
-              server = startServerWithPipes(
-                props.javaCmd,
-                props.dir,
-                props.fileName,
-                [],
-                props.jvmArgs
-              );
-              manager.server = server;
-            }, 250);
-          } else {
-            server.stdin.write(`${watch.cmd}\n`);
-          }
+          const now = Symbol();
+          watch.latest = now;
+          setTimeout(() => {
+            if (watch.latest !== now) return;
+            watch.md5Previous = currentMd5;
+            restarting = true;
+            console.log(`${filename} file Changed`);
+            fs.copyFileSync(watch.file, watch.out);
+            if (watch.cmd === "restart") {
+              console.log("Restarting server...");
+              server.kill();
+              setTimeout(() => {
+                server = startServerWithPipes(
+                  props.javaCmd,
+                  props.dir,
+                  props.fileName,
+                  [],
+                  props.jvmArgs
+                );
+                manager.server = server;
+              }, 250);
+            } else {
+              server.stdin.write(`${watch.cmd}\n`);
+            }
+          }, props.timeout);
         }
       });
     }
